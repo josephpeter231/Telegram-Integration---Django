0b7e401a1d97102de6876377cf9480f0a2694606
Joseph Peter J <110652580+josephpeter231@users.noreply.github.com>
2024-11-15 11:56:59 +0530

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import asyncio
 from telegram import Bot
 from telegram.ext import Application
 
-TOKEN = "7810923532:AAFiAXtlhvw7NjcJgWAUESMGctVrs9mHoT4"
+TOKEN = "your token "
 chat_id = 1814948590  # Replace with your actual chat ID
 
 async def send_message(text, chat_id):
